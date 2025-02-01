8209eaaf089dd6bd3ab8e05eb67caa72697a521f
Linnea Wolniewicz <linneamw@gn-17-23-00.mana.ci.its.hawaii.edu>
2025-01-31 17:01:33 -1000

    add explore_gp_parameters notebook

--- dipper/model_utils/gp_model.py
+++ dipper/model_utils/gp_model.py
@@ -59,12 +59,29 @@ def train_gp(
         training_iterations=1_000, 
         lr=0.01,
         device=torch.device("cpu"), 
+        which_metric="mll",
+        x_valid=None,
+        y_valid=None,
         likelihood=None,
         kernel=None,
         mean=None,
         early_stopping=True,
+        min_iterations=None,
+        patience=1,
         plot=False
     ):
+
+    if which_metric not in ["mll", "mse"]:
+        raise ValueError("which_metric must be either 'mll' or 'mse'.")
+
+    if early_stopping and min_iterations is None:
+        min_iterations = training_iterations // 10
+        print(f"Using {min_iterations} as minimum iterations for early stopping.")
+
+    if y_valid is None:
+        print("No validation data provided. Using training data for validation.")
+        x_valid = x_train
+        y_valid = y_train
 
     # Initialize likelihood
     if likelihood is not None:
@@ -103,66 +120,79 @@ def train_gp(
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
     
     # Plot loss during training
-    mll_losses = []
-    mse_losses = []
+    train_losses = []
+    valid_losses = []
     increase_count = 0
+
     for i in range(training_iterations):
         optimizer.zero_grad()
-        output = model(x_train)
-        mll_loss = -mll(output, y_train)
-        mse_loss = torch.nn.functional.mse_loss(output.mean, y_train)
+        pred = model(x_train)
+
+        model.eval()
+        with torch.no_grad():
+            valid_pred = model(x_valid)
+        model.train()
+
+        # Compute losses
+        if which_metric == "mse":
+            train_loss = torch.nn.functional.mse_loss(pred.mean, y_train)
+            valid_loss = torch.nn.functional.mse_loss(valid_pred.mean, y_valid)
+        else:
+            train_loss = -mll(pred, y_train)
+            valid_loss = -mll(valid_pred, y_valid)
 
         # Early stopping
         if early_stopping:
-            if i > 300 and mll_loss - mll_losses[i-1] > 0:
+            if i > min_iterations and valid_loss - valid_losses[i-1] > 0:
                 increase_count += 1
             
-            if increase_count > 5:
-                print(f"Early stopping at iteration {i} due to increasing loss.")
+            if increase_count > patience:
+                print(f"Early stopping at iteration {i} due to increasing valid loss.")
                 break
 
-        mll_loss.backward()
+        train_loss.backward()
         optimizer.step()
-        mll_losses.append(mll_loss.item())
-        mse_losses.append(mse_loss.item())
+
+        # Save losses
+        train_losses.append(train_loss.item())
+        valid_losses.append(valid_loss.item())
 
     if plot:
-        # Plot the loss
+        # Plot the train and valid loss side by side 
         plt.figure(figsize=(10,5))
-        plt.plot(range(len(mll_losses)), mll_losses)
+        plt.subplot(1, 2, 1)
+        plt.plot(range(len(train_losses)), train_losses)
         plt.xlabel("Iteration")
-        plt.ylabel("Loss")
-        plt.title("MLL Loss")
-        plt.show()
+        plt.ylabel("Train Loss")
+        plt.title("Train Loss for metric " + which_metric)
 
-        # Plot the loss
-        plt.figure(figsize=(10,5))
-        plt.plot(range(len(mse_losses)), mse_losses)
+        plt.subplot(1, 2, 2)
+        plt.plot(range(len(valid_losses)), valid_losses)
         plt.xlabel("Iteration")
-        plt.ylabel("Loss")
-        plt.title("MSE Loss")
+        plt.ylabel("Valid Loss")
+        plt.title("Valid Loss for metric " + which_metric)
         plt.show()
 
         # Plot the covariance matrices
         with torch.no_grad():
-            periodic_cov = model.covar_module.base_kernel.periodic_kernel(torch.tensor(x).to(device)).evaluate()
-            rbf_cov = model.covar_module.base_kernel.rbf_kernel(torch.tensor(x).to(device)).evaluate()
+            periodic_cov = model.covar_module.base_kernel.periodic_kernel(torch.tensor(x_train).to(device)).evaluate()
+            rbf_cov = model.covar_module.base_kernel.rbf_kernel(torch.tensor(x_train).to(device)).evaluate()
+            cov_matrix = model.covar_module(torch.tensor(x_train).to(device)).evaluate()
 
         plt.figure(figsize=(15, 5))
         plt.subplot(1, 3, 1)
+        plt.title("Covariance Matrix")
+        plt.imshow(cov_matrix.cpu().numpy(), cmap='viridis')
+        plt.colorbar()
+
+        plt.subplot(1, 3, 2)
         plt.title("Periodic Kernel Covariance")
         plt.imshow(periodic_cov.cpu().numpy(), cmap='viridis')
         plt.colorbar()
 
-        plt.subplot(1, 3, 2)
+        plt.subplot(1, 3, 3)
         plt.title("RBF Kernel Covariance")
         plt.imshow(rbf_cov.cpu().numpy(), cmap='viridis')
         plt.colorbar()
-
-        plt.subplot(1, 3, 3)
-        plt.title("RBF * Periodic Kernel Covariance")
-        plt.imshow(rbf_cov.cpu().numpy() * periodic_cov.cpu().numpy(), cmap='viridis')
-        plt.colorbar()
-        plt.show()
 
     return model, likelihood, mll
